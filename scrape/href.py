a86e4577690145eb6d1212954713fc7d0b91f582
RetxTan <74506417+RetxTan@users.noreply.github.com>
2023-01-22 01:39:54 +0800

    scrape

---
+++ scrape/href.py
@@ -0,0 +1,19 @@
+import requests
+
+url = 'https://www.fairprice.com.sg/categories'
+
+information = requests.get(url).text
+
+split_information = information.split(" ")
+
+categories = ""
+for item in split_information:
+    if 'href="/category/' in item:
+        intermediate = item.replace('href=', "").replace('"', "")
+        if categories != "":
+            categories += "\n"
+        categories += f"https://www.fairprice.com.sg{intermediate}"
+
+with open("links.txt", "w") as link_file:
+    link_file.write(categories)
+
