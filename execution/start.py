0da565c9fe24bee36ef639f8e5f8a9546fc3d7ac
RetxTan <74506417+RetxTan@users.noreply.github.com>
2023-02-05 21:32:00 +0800

    update

--- execution/start.py
+++ execution/start.py
@@ -1,6 +1,6 @@
 import os
 
-lst = os.listdir('execution/input')
+lst = os.listdir('/Users/retx/Documents/GitHub/National-AI-Challenge/execution/input')
 number_files = len(lst)
 print(number_files)
 # os.system("python receiptreader.py --image IMG_6548.JPG")
